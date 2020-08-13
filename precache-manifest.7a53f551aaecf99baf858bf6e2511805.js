051e259593c2c55477c2920c6cfb58614a16535a
wangxiong <wangxiong@china-revival.com>
2020-08-13 17:20:11 +0800

    init

---
+++ precache-manifest.7a53f551aaecf99baf858bf6e2511805.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "760183b7cc6d98580f27",
+    "url": "/static/css/main.323f4fed.chunk.css"
+  },
+  {
+    "revision": "760183b7cc6d98580f27",
+    "url": "/static/js/main.760183b7.chunk.js"
+  },
+  {
+    "revision": "5ac56422ea585d875d45",
+    "url": "/static/css/1.61a38b94.chunk.css"
+  },
+  {
+    "revision": "5ac56422ea585d875d45",
+    "url": "/static/js/1.5ac56422.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fb516c68ef9a28e45a3cded301923a13",
+    "url": "/static/media/mtfb.fb516c68.png"
+  },
+  {
+    "revision": "24e54b66e5d6c4ed0e2bd11c8517f1d0",
+    "url": "/static/media/card-warning.24e54b66.png"
+  },
+  {
+    "revision": "e28474765d60fc66b2ede9c56d6b1792",
+    "url": "/static/media/card-success.e2847476.png"
+  },
+  {
+    "revision": "3f053d3383eb1a890f131891702a65d8",
+    "url": "/static/media/card-primary.3f053d33.png"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "5084e8902e20ee2188f00ea333ec0a37",
+    "url": "/index.html"
+  }
+];
